25444edfe805bea733098f2715215f59c71a8297
kodewithdky <kodewithdky@gmail.com>
2024-09-17 01:52:55 +0530

    save changes

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -22,7 +22,7 @@ const sendVerificationMail = (name, email, userId) => {
          html:
             "<p>HI! " +
             name +
-            ', Please click here to <a href="https://namaskarhumanity.org/verify?id=' +
+            ', Please click here to <a href="https://humanity-server.onrender.com/api/v1/auth/verify?id=' +
             userId +
             '" target="_blank">Verify </a> your email. </p>',
       };
